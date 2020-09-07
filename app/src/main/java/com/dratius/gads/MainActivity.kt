4804e30eddab27ca1c5a03eb2d3b3ae3d153b6a5
Dratius <drasenick@gmail.com>
2020-09-08 00:45:45 +0300

    Initial Commit and load screen implementation

---
+++ app/src/main/java/com/dratius/gads/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.dratius.gads
+
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
+import android.graphics.drawable.AnimationDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.loader.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.loader)
+        val screenFilter = PorterDuffColorFilter(
+            Color.rgb(34,34,34),
+            PorterDuff.Mode.SCREEN
+        )
+        supporters.colorFilter = screenFilter
+        logo.colorFilter = screenFilter
+        banner.colorFilter = screenFilter
+        loader.colorFilter = screenFilter
+
+        loader.setBackgroundResource(R.drawable.loader_animation)
+        val load = loader.background as AnimationDrawable
+        load.start()
+
+    }
+}
